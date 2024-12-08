8ef20430c6592df88d6ea943511911ed76240ed8
Bienvo19 <kelvinbienve@gmail.com>
2024-12-08 00:56:15 -0400

    Corrigiendo errores

--- app.test.js
+++ app.test.js
@@ -1,5 +1,5 @@
-// app.test.js
-test('La página contiene "Hola Mundo"', () => {
+// tests/app.test.js
+test('La página contiene "Hola Mundo desde GitHub"', () => {
     const contenido = '<h1>Hola Mundo desde GitHub</h1>';
-    expect(contenido).toMatch(/Hola Mundo/);
+    expect(contenido).toMatch(/Hola Mundo desde GitHub/);
 });
